2cfb2bf23710806546d729a8aa26c328254ef741
Anjani Singh <anjanisingh953@gmail.com>
2025-12-13 23:55:50 +0530

    Add getter,setter, & virtual properties

--- models/user.js
+++ models/user.js
@@ -2,10 +2,27 @@ module.exports = (sequelize,DataTypes)=>{
  const User =   sequelize.define('User',{
         firstName:{
             type: DataTypes.STRING,
-            allowNull : false
+            allowNull : false,
+            get() {
+            const rawValue = this.getDataValue('firstName');
+            return rawValue ? rawValue.toUpperCase() : null;
+            }
+
         },
         lastName:{
             type: DataTypes.STRING,
+            set(value) {
+              this.setDataValue('lastName', value + ', Indian');
+            }
+        },
+        fullName: {
+            type: DataTypes.VIRTUAL,
+            get() {
+                return `${this.firstName} ${this.lastName}`;
+            },
+            set(value) {
+               throw new Error('Do not try to set the `fullName` value!');
+            }
         }
 
     },{
